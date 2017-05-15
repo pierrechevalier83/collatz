2f998c5612979cc5eef36025b2340fc1b6843d8f
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-05-16 00:23:10 +0100

    Cache previous findings
    One million points take 2.95 seconds.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 namespace collatz {
 
@@ -11,19 +13,51 @@ T next(T n) {
 }
 
 template <typename T>
-void sequence(T n) {
-    std::cout << n;
-    while (n != 1) {
-        n = next(n);
-        std::cout << " " << n;
+class Sequences {
+public:
+    bool add_chain(T n) {
+        chains.push_back({});
+        return add(n);
     }
-    std::cout << std::endl;
+    bool add(T n) {
+        if (!contains(n)) {
+            chains.back().push_back(n);
+            which_chain[n] = chains.size() - 1;
+            return n == 1;
+        }
+        return true;
+    }
+    void print_all() {
+        for (const auto& chain: chains) {
+            for (const auto& n: chain) {
+                std::cout << n << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+private:
+    bool contains(T n) {
+        return which_chain.find(n) != which_chain.end();
+    }
+    std::unordered_map<T, T> which_chain;
+    std::vector<std::vector<T>> chains;
+};
+
+template <typename T>
+void sequence(T n, Sequences<T>& seq) {
+    bool done = seq.add_chain(n);
+    while (!done) {
+        n = next(n);
+        done = seq.add(n);
+    }
 }
 
 } // namespace collatz
 
 int main() {
+    collatz::Sequences<int> seq;
     for (int i = 1; i < 10; ++i) {
-      collatz::sequence(i);
+      collatz::sequence(i, seq);
     }
+    seq.print_all();
 }
